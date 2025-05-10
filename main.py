85a7052c7cf41e7b571c5a744ac61c5f205b2be1
Abstract High-level Algorithms <abdulraheemabdullah859@gmail.com>
2025-05-10 17:22:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ tokenizer = T5Tokenizer.from_pretrained("./text_summarizer")
 # Function to summarize text
 def generate_summary(text):
     # Encode the input text
-    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=512, truncation=True)
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=1024, truncation=True)
     
     # Generate summary
-    summary_ids = model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary_ids = model.generate(inputs, max_length=1024, min_length=150, length_penalty=2.0, num_beams=4, early_stopping=True)
     
     # Decode the summary
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
@@ -23,10 +23,12 @@ st.header("This app summarizes text using a pre-trained T5 model.")
 st.write("Enter the text you want to summarize below:")
 text_input = st.text_area("Input Text", height=300, placeholder="Paste your text here...")
 if st.button("Generate Summary"):
-    if text_input:
+    if not text_input:
+        st.warning("Please enter some text to summarize.")
+    elif len(text_input.strip()) < 150:
+        st.warning("Please enter at least 210 characters for summarization.")
+    else:
         with st.spinner("Generating summary..."):
             summary = generate_summary(text_input)
-        st.subheader("Generated Summary:")
-        st.write(summary)
-    else:
-        st.warning("Please enter some text to summarize.")
+            st.subheader("Generated Summary:")
+            st.write(summary)
